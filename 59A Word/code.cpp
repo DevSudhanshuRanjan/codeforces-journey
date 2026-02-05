374dd03642c82f100bf4d232091c841508f5c1b3
SUDHANSHU RANJAN <code.sudhanshu.ranjan@gmail.com>
2026-02-05 16:22:37 +0530

    Add problem statement and solution for Codeforces problem 59A: Word

---
+++ 59A Word/code.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string s;
+    cin >> s;
+    int lower = 0, upper = 0;
+    for (char c : s) {
+        if (islower(c)) lower++;
+        else upper++;
+    }
+    if(lower >= upper){
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+    } else transform(s.begin(),s.end(),s.begin(),::toupper);
+    cout << s << endl;
+
+    return 0;
+}
