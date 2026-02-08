a86bc951a7055f1eb2bb977fad09a08ee9622c51
SUDHANSHU RANJAN <code.sudhanshu.ranjan@gmail.com>
2026-02-09 00:40:05 +0530

    Add problem statement and solution for Codeforces problem 2198A: Lawn Mower

---
+++ 2198A Lawn Mower/code.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+ 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, w;
+        cin >> n >> w;
+        cout << n - (n / w)<<endl;
+    }
+ 
+    return 0;
+}
