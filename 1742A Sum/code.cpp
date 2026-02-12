a9c196265f4c3d0629f3a2cf79a174795d6b908b
SUDHANSHU RANJAN <code.sudhanshu.ranjan@gmail.com>
2026-02-12 15:34:30 +0530

    Add problem statement and solution for Codeforces problem 1742A: Sum

---
+++ 1742A Sum/code.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+b==c or a+c==b or b+c==a)cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+
+    }
+
+    return 0;
+}
