33b1cc7bb8841ae4c187604243d65afe9f76828b
SUDHANSHU RANJAN <code.sudhanshu.ranjan@gmail.com>
2026-02-26 13:46:00 +0530

    Add problem statement and solution for Codeforces problem 1829A: Blank Space

---
+++ 1829A Blank Space/code.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        int n;
+    cin >> n;
+    int a[n];
+    int cnt = 0, ans = 0;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        if(a[i] == 0)
+        {
+            cnt++;
+        }
+        else
+        {
+            ans = max(ans, cnt);
+            cnt = 0;
+        }
+    }
+    cout << max(ans, cnt) << endl;
+    }
+
+    return 0;
+}
